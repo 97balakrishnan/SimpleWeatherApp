e0d1706f471359b1cf34ebc629b8a1854a4ac762
97balakrishnan <sbalakrishnan15093@it.ssn.edu.in>
2018-02-20 22:57:45 +0530

    First commit

---
+++ app/src/main/java/com/example/balakrishnan/simpleweather/GPSTracker.java
@@ -0,0 +1,215 @@
+package com.example.balakrishnan.simpleweather;
+
+/**
+ * Created by balakrishnan on 20/2/18.
+ */
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
+
+import android.app.AlertDialog;
+import android.app.Service;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.os.IBinder;
+import android.provider.Settings;
+import android.util.Log;
+
+/**
+ * Create this Class from tutorial :
+ * http://www.androidhive.info/2012/07/android-gps-location-manager-tutorial
+ *
+ * For Geocoder read this : http://stackoverflow.com/questions/472313/android-reverse-geocoding-getfromlocation
+ *
+ */
+
+public class GPSTracker extends Service implements LocationListener {
+
+    // Get Class Name
+    private static String TAG = GPSTracker.class.getName();
+
+    private final Context mContext;
+
+    // flag for GPS Status
+    boolean isGPSEnabled = false;
+
+    // flag for network status
+    boolean isNetworkEnabled = false;
+
+    // flag for GPS Tracking is enabled
+    boolean isGPSTrackingEnabled = false;
+
+    Location location;
+    public static double latitude;
+    public static double longitude;
+
+    // How many Geocoder should return our GPSTracker
+    int geocoderMaxResults = 1;
+
+    // The minimum distance to change updates in meters
+    private static final long MIN_DISTANCE_CHANGE_FOR_UPDATES = 10; // 10 meters
+
+    // The minimum time between updates in milliseconds
+    private static final long MIN_TIME_BW_UPDATES = 1000 * 60 * 1; // 1 minute
+
+    // Declaring a Location Manager
+    protected LocationManager locationManager;
+
+    // Store LocationManager.GPS_PROVIDER or LocationManager.NETWORK_PROVIDER information
+    private String provider_info;
+
+    public GPSTracker(Context context) {
+        this.mContext = context;
+        getLocation();
+    }
+
+    /**
+     * Try to get my current location by GPS or Network Provider
+     */
+    public void getLocation() {
+
+        try {
+            locationManager = (LocationManager) mContext.getSystemService(LOCATION_SERVICE);
+
+            //getting GPS status
+            isGPSEnabled = locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
+
+            //getting network status
+            isNetworkEnabled = locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER);
+
+            // Try to get location if you GPS Service is enabled
+            if (isGPSEnabled) {
+                this.isGPSTrackingEnabled = true;
+
+                Log.d(TAG, "Application use GPS Service");
+
+				/*
+				 * This provider determines location using
+				 * satellites. Depending on conditions, this provider may take a while to return
+				 * a location fix.
+				 */
+
+                provider_info = LocationManager.GPS_PROVIDER;
+
+            } else if (isNetworkEnabled) { // Try to get location if you Network Service is enabled
+                this.isGPSTrackingEnabled = true;
+
+                Log.d(TAG, "Application use Network State to get GPS coordinates");
+
+				/*
+				 * This provider determines location based on
+				 * availability of cell tower and WiFi access points. Results are retrieved
+				 * by means of a network lookup.
+				 */
+                provider_info = LocationManager.NETWORK_PROVIDER;
+
+            }
+            try{
+            // Application can use GPS or Network Provider
+            if (!provider_info.isEmpty()) {
+                locationManager.requestLocationUpdates(
+                        provider_info,
+                        MIN_TIME_BW_UPDATES,
+                        MIN_DISTANCE_CHANGE_FOR_UPDATES,
+                        this
+                );
+
+                if (locationManager != null) {
+                    location = locationManager.getLastKnownLocation(provider_info);
+                    updateGPSCoordinates();
+                }
+            }}catch(SecurityException s)
+            {
+                System.out.println("Security Exception");
+            }
+        }
+        catch (Exception e)
+        {
+            //e.printStackTrace();
+            Log.e(TAG, "Impossible to connect to LocationManager", e);
+        }
+    }
+
+    /**
+     * Update GPSTracker latitude and longitude
+     */
+    public void updateGPSCoordinates() {
+        if (location != null) {
+            latitude = location.getLatitude();
+            longitude = location.getLongitude();
+        }
+    }
+
+    /**
+     * GPSTracker latitude getter and setter
+     * @return latitude
+     */
+    public double getLatitude() {
+        if (location != null) {
+            latitude = location.getLatitude();
+        }
+
+        return latitude;
+    }
+
+    /**
+     * GPSTracker longitude getter and setter
+     * @return
+     */
+    public double getLongitude() {
+        if (location != null) {
+            longitude = location.getLongitude();
+        }
+
+        return longitude;
+    }
+
+    /**
+     * GPSTracker isGPSTrackingEnabled getter.
+     * Check GPS/wifi is enabled
+     */
+    public boolean getIsGPSTrackingEnabled() {
+
+        return this.isGPSTrackingEnabled;
+    }
+
+    /**
+     * Stop using GPS listener
+     * Calling this method will stop using GPS in your app
+     */
+    public void stopUsingGPS() {
+        if (locationManager != null) {
+            locationManager.removeUpdates(GPSTracker.this);
+        }
+    }
+
+
+    @Override
+    public void onLocationChanged(Location location) {
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+}
